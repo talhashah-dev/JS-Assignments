3ecaf5b6ca999078bf25ea6f1bb0d30e3e0b5d61
Syed Talha <syedt.in00@gmail.com>
2023-10-06 22:42:52 +0500

    updated js file

--- Assignments/Assignment 2 chp 17 to 67/app.js
+++ Assignments/Assignment 2 chp 17 to 67/app.js
@@ -1,4 +1,36 @@
 // // =================== Javascript Assignment 2 Chapter 17 to 67 ==========================
 
-// // -\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-- Chapter 1 --\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-
-// // Question-1
+// // -\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-- Chapter 17 --\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-
+// // ---------- Question-1
+// let array = [
+//     [],
+//     [],
+//     []
+// ];
+// console.log(array)
+// // ---------- Question-2
+// let matrixArray = [
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1]
+// ];
+// console.log(matrixArray)
+
+// // ---------- Question-3
+// for (let i=1;i<=10;i++){
+//     console.log(i)
+// };
+
+// // ---------- Question-4
+// let number = +prompt("Enter a number to show it's multiplication table");
+// let length = +prompt("Enter  length of multiplication table");
+// console.log(`Multiplication table of ${number}`)
+// console.log(`Length ${length}`)
+// for (let i=1;i<= length;i++){
+//     let result = length * i;
+//     console.log(`${number} x ${i} = ${result}`)
+// };
+
+// // ---------- Question-5
+
+
